e113a16cb1dc748c1e7ba91e35225e2969939571
Niek Palm <dev.npalm@gmail.com>
2020-08-12 10:21:48 +0200

    Add issue template

---
+++ .github/notify.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+ISSUE_TITLE=$1
+ISSUE_URL=$2
+SLACK_WEBHOOK=$3
+curl -X POST -H 'Content-type: application/json' --data '{"text":"New issue $ISSUE_TITLE opened, $ISSUE_URL"}' $SLACK_WEBHOOK
